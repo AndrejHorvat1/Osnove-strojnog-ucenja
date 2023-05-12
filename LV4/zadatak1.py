a82504500fbc92404fb3bc987b5ac3f1db6a8427
AndrejHorvat1 <127336594+AndrejHorvat1@users.noreply.github.com>
2023-05-12 02:41:27 +0200

    Update zadatak1.py

--- LV4/zadatak1.py
+++ LV4/zadatak1.py
@@ -9,6 +9,18 @@ import sklearn . linear_model as lm
 from sklearn import metrics
 data=pd.read_csv('data_C02_emission.csv')
 
+
+# podaci iz drugacijih datoteka se mogu ucitati i s:
+# somedata = datasets.name_of_dataset()
+# df = pd.DataFrame(data=somedata.data, columns=somedata.feature_names)
+# df['target'] = somedata.target
+# df.to_csv('somedata.csv', index=False)
+# print(df.info)
+
+
+# drop sve kategoriske velicine tj one s tekstom
+data = data.drop(["Make", "Model"], axis=1)
+
 input_variables=['Fuel Consumption City (L/100km)',
                  'Fuel Consumption Hwy (L/100km)',
                  'Fuel Consumption Comb (L/100km)',
